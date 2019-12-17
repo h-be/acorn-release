60bb04cf64dc94b24922c1efc3f8112755ab9892
David Meister <thedavidmeister@gmail.com>
2019-12-17 21:08:41 +0400

    add acorn-reset

--- nix/acorn/default.nix
+++ nix/acorn/default.nix
@@ -9,17 +9,22 @@ let
   ./clean.sh
  '');
 
+ reset = (pkgs.writeShellScriptBin "acorn-reset" ''
+  ./reset.sh
+ '');
+
  build = (pkgs.writeShellScriptBin "acorn-build" ''
   set -euxo pipefail
   acorn_platform=''${1:-linux}
   acorn_arch=''${2:-x64}
   ${update-deps}/bin/acorn-update-deps
-  ./clean.sh
+  ${clean}/bin/acorn-clean
   electron-packager . Acorn --platform=$acorn_platform --arch=$acorn_arch --overwrite
   chmod +x ./Acorn-$acorn_platform-$acorn_arch/Acorn
  '');
 
  acorn = (pkgs.writeShellScriptBin "acorn" ''
+  ${reset}/bin/acorn-reset
   ${update-deps}/bin/acorn-update-deps
   ${pkgs.electron_6}/bin/electron .
  '');
@@ -28,6 +33,7 @@ in
  buildInputs = [
   update-deps
   clean
+  reset
   build
   acorn
  ];
