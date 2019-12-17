7568090d4c720faa0397b81981a8b0bbd6e3ffe0
David Meister <thedavidmeister@gmail.com>
2019-12-17 20:50:21 +0400

    differentiate between clean and reset

--- reset.sh
+++ reset.sh
@@ -2,5 +2,9 @@
 set -euxo pipefail
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "${DIR}"
-rm -rf ./acorn-ui
-rm -rf ./acorn-hc
+./clean.sh
+rm -rf ./ui
+rm -rf ./dna
+rm -rf ./Acorn-*
+rm -rf $HOME/.config/Acorn
+rm -rf ./node_modules
