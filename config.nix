e9a811444069741b5916a250e891b52f2607407b
Connor Turland <connorturland@gmail.com>
2020-08-04 11:54:09 -0400

    change nix config numbers

--- config.nix
+++ config.nix
@@ -68,8 +68,8 @@ echo "All finished!!!"
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.0.1";
-   previous = "_._._";
+   current = "0.3.4";
+   previous = "0.3.3";
   };
 
   github = {
